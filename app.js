60c2b4b7761e351b1953f27b6742f996949c5386
Michael Chop <007micle007@gmail.com>
2022-11-11 09:37:24 +0300

    Configured authorization routes

--- app.js
+++ app.js
@@ -2,21 +2,20 @@ const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
 
+const PORT = config.get('port') || 5000
 const app = express()
 
-const PORT = config.get('port') || 5000
+app.use('/api/auth', require('./routes/auth.routes'))
 
 async function start() {
     try {
-        await mongoose.connect(config.get('mongoURI'), {
-            
-        })
+        await mongoose.connect(config.get('mongoURI'))
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
     } catch(e) {
-        console.log('Server Error', e.message)
+        console.log('Server Error:', e.message)
         process.exit(1)
     }
 }
 
 start()
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
