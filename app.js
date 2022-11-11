71057cfe92d4d6cd6ad976621d08e03182568bab
Michael Chop <007micle007@gmail.com>
2022-11-12 00:15:44 +0300

    Added Link methods on the server

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routes'))
+app.use('/api/link', require('./routes/link.routes'))
 
 async function start() {
     try {
---
+++ routes/link.routes.js
@@ -0,0 +1,52 @@
+const {Router} = require('express')
+const Link = require('../models/Link')
+const auth = require('middleware/auth.middleware')
+const config = require('config')
+const shortid = require('shortid')
+const router = Router()
+
+router.post('/generate', auth, async (req, res) => {
+    try {
+        const baseURL = config.get('baseURL')
+        const {from} = req.body
+
+        const code = shortid.generate()
+        const existing = await Link.findOne({from})
+
+        if(existing) {
+            return res.json({ link: existing})
+        }
+
+        const to = baseURL + '/t/' + code
+
+        const link = new Link({
+            code, to, from, owner: req.user.userId
+        })
+
+        await link.save()
+
+        res.status(201).json({ link })
+    } catch(e) {
+        res.status(500).json({message: 'Что-то пошло не так'})
+    }
+})
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const links = await Link.find({ owner: req.user.userId })
+        res.json(links)
+    } catch(e) {
+        res.status(500).json({message: 'Что-то пошло не так'})
+    }
+})
+
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const link = await Link.findById(req.params.id)
+        res.json(link)
+    } catch(e) {
+        res.status(500).json({message: 'Что-то пошло не так'})
+    }
+})
+
+module.exports = router
