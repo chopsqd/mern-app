2730c27ee8397f0ff0c3c76b78e53f03104f6f9a
Michael Chop <007micle007@gmail.com>
2022-11-12 01:37:34 +0300

    Implemented the redirect functionality

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
+app.use('/t', require('./routes/redirect.routes'))
 
 async function start() {
     try {
